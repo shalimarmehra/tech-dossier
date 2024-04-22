9089a1080a8ae1e6f15e6fcbdd8905ab21b53a12
shalimar-mehra <shalimarmehraa@gmail.com>
2024-04-22 21:11:48 +0530

    add commentsection to the post page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,10 +14,12 @@ import OnlyAdminPrivateRoute from "./Components/OnlyAdminPrivateRoute";
 import CreatePost from "./Pages/CreatePost";
 import UpdatePost from "./Pages/UpdatePost";
 import PostPage from "./Pages/PostPage"
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
